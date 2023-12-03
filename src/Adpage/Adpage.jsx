1f8b2c7cdf128c1f5546fcc3e531b334d6aa2d4f
Piyush gupta <63692513+piyush853@users.noreply.github.com>
2023-12-03 12:16:59 +0530

    initial commit

---
+++ src/Adpage/Adpage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '/Users/piyushgupta/onlyads/src/Adpage/adpage.css'
+function Adpage() {
+  return (
+    <div>
+        <h1 className='heading'>
+            Welcome to only ads,You can advertise your brand here
+        </h1>
+        <h2 className='smallHeading'>Kindly send your ad details and pic on this mail id onlyads36@gmail.com (AND PAYMENT)</h2>
+        <h2 className='smallHeading'>CHARGES FOR 1 GRID(80 X 80) IS Rs. 399 for 24 hours </h2>
+    </div>
+  )
+}
+
+export default Adpage
